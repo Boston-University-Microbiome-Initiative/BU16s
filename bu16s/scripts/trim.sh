fc5f4b8574d2820c870b8ba2e29dfa819d31713d
Michael Silverstein <msilverstein@finchtherapeutics.com>
2020-06-15 19:47:06 -0400

    test scripts

---
+++ bu16s/scripts/trim.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -l
+
+#$ -P talbot-lab-data
+#$ -j y#!/usr/bin/env bash
+
+PARAMETERS=$1
+source $PARAMETERS
+source $RUNPARAMETERS
+
+trim_output=$INTERMEDIATEDIR/"$PROJECTNAME"_trimmed.qza
+if [ $PAIRED == "true" ]
+then
+    trim_setting="trim-paired"
+    front_option="p-front-f"
+    rev_option="p-front-r"
+else
+    trim_setting="trim-single"
+    front_option="p-front"
+    rev_option="p-anywhere"
+fi
+
+cmd="qiime cutadapt $trim_setting \
+    --i-demultiplexed-sequences $demux_artifact \
+    --$front_option $FWD_PRIMER \
+    --$rev_option $REV_PRIMER \
+    --p-error-rate 0.1 \
+    --p-indels True \
+    --p-times 2 \
+    --p-overlap 3 \
+    --p-match-read-wildcards True \
+    --p-match-adapter-wildcards True \
+    --p-minimum-length 50 \
+    --p-cores $(nproc) \
+    --verbose \
+    --o-trimmed-sequences $trim_output"
+echo --------------------------------------------------------------------------------------
+echo $cmd
+echo --------------------------------------------------------------------------------------
+echo
+eval $cmd
+echo
+
+# Add to run parameters
+echo "export trim_output=$trim_output" >> $RUNPARAMETERS
