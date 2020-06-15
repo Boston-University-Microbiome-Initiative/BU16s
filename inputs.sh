202a61c13aba94d8f81dff2d788231f2c841de96
Michael Silverstein <msilverstein@finchtherapeutics.com>
2020-06-15 13:05:05 -0400

    full paths

--- inputs.sh
+++ inputs.sh
@@ -2,13 +2,14 @@
 # THIS MUST BE UPDATED FOR YOUR PROJECT
 
 # I/O **NO TRAILING SLASHES!
-export INPUTDIR=test/files
-export OUTPUTDIR=test
+export INPUTDIR=/projectnb/talbot-lab-data/msilver/BU16s/test/files
+export OUTPUTDIR=/projectnb/talbot-lab-data/msilver/BU16s/test
 
 export FWD_FMT=-r1.fastq.gz
 export REV_FMT=-r2.fastq.gz
 
 
 # PIPELINE PARAMETERS
-# THESE SHOULD ONLY BE CHANGED FOR CHANGES TO OODE
-export SCRIPTSDIR=bu16s/scripts
+# THESE SHOULD ONLY BE CHANGED FOR CHANGES TO CODE
+export SCRIPTSDIR=/projectnb/talbot-lab-data/msilver/BU16s/bu16s/scripts
+
